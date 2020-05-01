7a17d01cf525bb25467857f6c66f3f57c0622249
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2020-05-02 03:20:38 +0900

    main.c 추가
    minitest.c를 구현하기 위해서 필요한 main.c를 만들었습니다.
    완성된 것은 아니고 listRes, add, update, readMenu까지 작동이 되도록
    만들었습니다
    구조체 포인터 배열기반으로 운영이 되게 하였습니다...
    
    2020/05/02
    작성자: 백주원

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"miniproject.h"
+
+int main(void){
+
+int menu, count = 0;
+
+Restaurant *res[100];
+
+while(1){
+readMenu();
+printf("메뉴를 선택해주세요!\n");
+scanf("%d",&menu);
+
+if(menu ==1) {addData(&res[count]); count++;}
+else if(menu == 2) {listRes(res,count);}
+else if(menu == 3) {updateData(res,count);}
+else if( menu == 0) break;
+else printf("다시입력해주세요!\n");
+}
+
+
+}
